f94508136075660b20842a9235f6dc85fa68d8ee
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-19 09:43:17 +0100

    update

--- cours/bases/index.js
+++ cours/bases/index.js
@@ -1,13 +1,66 @@
 // alt + shift + F    ->    prettier
 
-const first_name = "Geoffroy"
-let age = 39
+// const first_name = "Geoffroy";
+// let age = 39;
 
-let nb = 5;
-nb += 5;
+// let nb = 5;
+// nb += 5;
 
-console.log("log !!")
-console.error("error !!")
-console.warn("warn !!")
-console.info("info !!")
+// console.log(nb ** 2 % 3);
 
+// let counter = 1;
+// let value = ++counter;
+
+// counter++;
+
+// console.log("counter", counter)
+// console.log("value", value);
+
+// console.log(`counter: ${counter}`);
+
+
+// let firstName = "Geoffroy";
+
+// console.log(firstName.length)
+
+// let spy = "James Dubois"
+// let realSpy = spy.replace("Dubois", "Bond")
+
+// console.log(realSpy);
+
+// console.log("Bonjour" + " " + realSpy);
+// console.log(`Bonjour ${realSpy}`);
+
+// let res;
+
+// res = realSpy.split()
+// res = realSpy.length
+
+// let mineur = true;
+
+// false == 0
+// false == ""
+// false == undefined
+// false == null
+// false == NaN
+// false == []
+
+// null
+// undefined
+
+// let age;
+
+// console.log(age == undefined);
+
+// age = 39;
+
+// age = null;
+
+// console.log(typeof undefined);
+// console.log(typeof 0);
+// console.log(typeof 10n);
+// console.log(typeof true);
+// console.log(typeof "foo");
+// console.log(typeof Math);
+// console.log(typeof null);
+// console.log(typeof alert);
