934c58524416a655ed7b3f1358ab4121e8816ce8
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-19 11:51:45 +0100

    update

--- exercices/nombre_mystere/index.js
+++ exercices/nombre_mystere/index.js
@@ -1,5 +1,30 @@
-"use strict"
+"use strict";
 
 console.log("Jeu mystère...");
 
-console.log(Math.floor(Math.random() * 28)) + 1;
+const nb = Math.floor(Math.random() * 100) + 1;
+let input;
+let history = [];
+let max = 100;
+let min = 1;
+
+while (input != nb) {
+  input = +prompt(`Entrez un nombre entre ${min} et ${max}:`);
+  history.push(input);
+
+  if (input < min || input > max) {
+    continue;
+  }
+
+  if (input < nb) {
+    console.log("Trop petit !");
+    min = input;
+  } else if (input > nb) {
+    console.log("Trop grand !");
+    max = input;
+  } else {
+    console.log(`Bravo, vous avez trouvé le nombre ${nb}`);
+    console.log("Vos tentatives:", history);
+    break;
+  }
+}
