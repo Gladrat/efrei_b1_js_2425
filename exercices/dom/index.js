3854b9abd3b15127829b86fcea7758404d4b19ba
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-21 11:43:13 +0100

    update

---
+++ exercices/dom/index.js
@@ -0,0 +1,30 @@
+// 1.
+// const table = document.querySelector("#age-table");
+const table = document.querySelector("table");
+console.log(table);
+
+// 2.
+// const label = document.querySelectorAll("#age-table label")
+const labelList = table.querySelectorAll("label");
+console.log(labelList);
+
+// 3.
+// const firstTD = table.querySelector("td");
+const firstTD = table.rows[0].cells[0]
+console.log(firstTD);
+
+// 4.
+const form = document.querySelector("form[name=search]")
+console.log(form);
+
+// 5.
+const firstInput = form.querySelector("input");
+console.log(firstInput);
+
+// 6.
+// const lastInput = form.querySelectorAll("input")[form.querySelectorAll("input").length-1];
+const lastInput = form.children[form.children.length-1]
+console.log(lastInput);
+
+// 7.
+table
