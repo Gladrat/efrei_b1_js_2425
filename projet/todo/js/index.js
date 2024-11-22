1de1350db7f2248ff518308affffa9847a9c1906
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-22 09:06:36 +0100

    update

--- projet/todo/js/index.js
+++ projet/todo/js/index.js
@@ -1,14 +1,25 @@
 "use strict";
 
-// Récupérer les éléments HTML correspondants
-const list = "";
-const input = "";
-const add = "";
-const clear = "";
-const load = "";
+const list = document.querySelector("#list");
+const input = document.querySelector("#input");
+const add = document.querySelector("#add");
+const clear = document.querySelector("#clear");
+const load = document.querySelector("#load");
 
-// Parcourir le tableau des tâches (boucle)
-  // Tester si la tâche est une chaine non-nulle
-    // Rajouter la tâche dans le DOM (dans le ul sous forme d'un li pour chaque tâche)
-    // Rajouter en + pour chaque tâche un bouton "REMOVE"
 let tasks = ["Apprendre le C++", "Faire les courses", "Changer une ampoule"];
+
+let i = 0;
+for (let t of tasks) {
+  if (t && typeof t === "string") {
+    const li = document.createElement("li");
+    const remove = document.createElement("button");
+
+    li.textContent = t;
+    remove.textContent = "REMOVE";
+
+    li.append(remove);
+    list.append(li);
+
+    i++;
+  }
+}
