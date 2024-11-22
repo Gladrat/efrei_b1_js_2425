1ec92be1ff3ac64e903e986b6d8b725027b17bf8
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-22 08:27:37 +0100

    update

--- projet/todo/js/index.js
+++ projet/todo/js/index.js
@@ -1,8 +1,14 @@
-"use strict"
+"use strict";
 
+// Récupérer les éléments HTML correspondants
 const list = "";
 const input = "";
 const add = "";
 const clear = "";
 const load = "";
+
+// Parcourir le tableau des tâches (boucle)
+  // Tester si la tâche est une chaine non-nulle
+    // Rajouter la tâche dans le DOM (dans le ul sous forme d'un li pour chaque tâche)
+    // Rajouter en + pour chaque tâche un bouton "REMOVE"
 let tasks = ["Apprendre le C++", "Faire les courses", "Changer une ampoule"];
