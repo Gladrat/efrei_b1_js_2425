3a5b5378e10fcc6fa2feadb68f6dc0691b3fd8fa
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-11-26 10:43:59 +0100

    update

--- cours/APIStorage/index.js
+++ cours/APIStorage/index.js
@@ -1,26 +1,29 @@
 console.log("*API Storage*");
 
-let theme = "light" // "dark";
+if (localStorage.key("theme") === null) {
+  localStorage.setItem("theme", "light");
+}
+
+let theme = localStorage.getItem("theme");
 
 const lightButton = document.querySelector("#light");
 const darkButton = document.querySelector("#dark");
 const h1 = document.querySelector("h1");
 
 h1.textContent = `Theme: ${theme}`;
+document.body.className = theme;
 
 lightButton.addEventListener("click", () => {
   theme = "light";
+
   h1.textContent = `Theme: ${theme}`;
   document.body.className = "light";
-})
+  localStorage.setItem("theme", theme);
+});
 darkButton.addEventListener("click", () => {
   theme = "dark";
+
   h1.textContent = `Theme: ${theme}`;
   document.body.className = "dark";
-})
-
-// API Storage
-
-localStorage
-sessionStorage
-
+  localStorage.setItem("theme", theme);
+});
